89302eb7cd159e0517c5730a93227d4226afc62b
YinZong <kevin47695@gmail.com>
2018-10-24 11:21:53 +0800

    Add function

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var multer = require('multer');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = 'Images';
 var app = express();
 const { spawn } = require('child_process');
 
+
 app.use(bodyParser.json());
+
+function cmdRun(){
+	var existFile = new Array(".jpg", ".jpeg");
+
+	fs.readdir(path, function(err, files){
+		if(err){
+			console.log(err);
+		}
+		var ls = spawn('jpg2dcm', ['./Images/' + files[0], './dcmFiles/3456.dcm']);
+		console.log(files[0]);
+	});
+}
 
 var Storage = multer.diskStorage({
 	destination: function(req, file, callback){
@@ -16,6 +31,7 @@ var Storage = multer.diskStorage({
 });
 
 var upload = multer({storage: Storage}).single("ImgUploader");
+
 
 app.get("/", function(req, res){
 	res.sendFile(__dirname + "/index.html");
@@ -29,6 +45,11 @@ app.post("/upload", function(req, res){
 		// return res.end("File upload sucessfully!");
 		return res.sendFile(__dirname + "/upload.html");
 	});
+});
+
+app.get("/Convert", function(req, res){
+	cmdRun();
+	return res.end("DONE!");
 });
 
 app.listen(3030, function(a){
